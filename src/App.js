d50b61a664331196b947fe919481a1886a3a3b8f
Manuel Torrez <manuel.torrez.vado@protonmail.com>
2018-12-28 16:58:06 -0600

    Creating a functional component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
@@ -7,6 +8,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>Is this working?</p>
+        <Person />
       </div>
     );
   }
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const person = () => {
+    return <h1>I'm a person, I guess.</h1>
+}
+
+export default person;
