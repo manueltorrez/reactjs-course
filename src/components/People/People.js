03ae3777ef8b2e869eaebe27dc0452a3673c54b5
Manuel Torrez <manuel.torrez.vado@protonmail.com>
2019-01-04 14:49:27 -0600

    Converting stateless to stateful components

--- src/components/People/People.js
+++ src/components/People/People.js
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
 import Person from "./Person/Person";
 
-const people = props =>
-  props.people.map((person, index) => {
-    return (
-      <Person
-        name={person.name}
-        age={person.age}
-        key={person.id}
-        click={() => props.clicked(index)}
-        changed={event => props.changed(event, person.id)}
-      />
-    );
-  });
+class People extends Component {
+  render() {
+    return this.props.people.map((person, index) => {
+        return (
+          <Person
+            name={person.name}
+            age={person.age}
+            key={person.id}
+            click={() => this.props.clicked(index)}
+            changed={event => this.props.changed(event, person.id)}
+          />
+        );
+      });
+  }
+}
 
-export default people;
+export default People;
--- src/components/People/Person/Person.js
+++ src/components/People/Person/Person.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "./Person.module.css";
 
-const person = props => {
-  return (
-    <div className={classes.Person}>
-      <p onClick={props.click}>
-        I'm a {props.name}, I guess. And I am {props.age} years old!
-      </p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name} />
-    </div>
-  );
-};
+class Person extends Component {
+  render() {
+    return (
+      <div className={classes.Person}>
+        <p onClick={this.props.click}>
+          I'm a {this.props.name}, I guess. And I am {this.props.age} years old!
+        </p>
+        <p>{this.props.children}</p>
+        <input type="text" onChange={this.props.changed} value={this.props.name} />
+      </div>
+    );
+  }
+}
 
-export default person;
+export default Person;
