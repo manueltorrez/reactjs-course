0d3fd50fb87ba161313e26fe4b2c03ee5a09bc3a
Manuel Torrez <manuel.torrez.vado@protonmail.com>
2019-01-04 15:37:19 -0600

    Updating lifecycle hooks triggered by state changes

--- src/components/People/People.js
+++ src/components/People/People.js
@@ -15,6 +15,23 @@ class People extends Component {
     console.log("[People.js] Inside componentDidMount");
   }
 
+  componentWillReceiveProps(nextProps) {
+    console.log('[UPDATE People.js] Inside componentWillReceiveProps', nextProps);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[UPDATE People.js] Inside shouldComponentUpdate', nextProps, nextState);
+    return nextProps.people !== this.props.people;
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log('[UPDATE People.js] Inside componentWillUpdate', nextProps, nextState);
+  }
+  
+  componentDidUpdate() {
+    console.log('[UPDATE People.js] Inside componentDidUpdate');
+  }
+
   render() {
     console.log('[People.js] Inside render()');
     return this.props.people.map((person, index) => {
