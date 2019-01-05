ea6ef0b359a4d8b9e6d6a555061a689409aaf2c8
Manuel Torrez <manuel.torrez.vado@protonmail.com>
2019-01-05 12:18:31 -0600

    Using References with React.createReference

--- src/components/People/Person/Person.js
+++ src/components/People/Person/Person.js
@@ -8,6 +8,7 @@ class Person extends Component {
   constructor(props) {
     super(props);
     console.log("[Person.js] Inside constructor", props);
+    this.inputElement = React.createRef();
   }
 
   componentWillMount() {
@@ -17,7 +18,7 @@ class Person extends Component {
   componentDidMount() {
     console.log("[Person.js] Inside componentDidMount");
     if (this.props.position === 0) {
-      this.inputElement.focus();
+      this.inputElement.current.focus();
     }
   }
 
@@ -31,9 +32,7 @@ class Person extends Component {
         <p>{this.props.children}</p>
         <input
           type="text"
-          ref={inp => {
-            this.inputElement = inp;
-          }}
+          ref={this.inputElement}
           onChange={this.props.changed}
           value={this.props.name}
         />
